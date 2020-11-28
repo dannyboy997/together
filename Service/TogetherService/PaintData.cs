c0820cf32a682dad07353df9d464cb2d77f4b231
Daniel Hagen <dannyboy997@outlook.com>
2020-11-27 18:01:03 -0800

    init

---
+++ Service/TogetherService/PaintData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TogetherService
+{
+    public class PaintData
+    {
+        public PaintLine[] Line { get; set; }
+
+        public string UserId { get; set; }
+    }
+
+    public class PaintLine
+    {
+        public PaintCoordinate Start { get; set; }
+
+        public PaintCoordinate Stop { get; set; }
+    }
+
+    public class PaintCoordinate
+    {
+        public int OffsetX { get; set; }
+
+        public int OffsetY { get; set; }
+    }
+}
